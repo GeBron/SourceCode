fa8a01ec295b87ffd7b572f13483468d59063ffd
GeBron <gebron@qq.com>
2020-08-10 16:55:13 +0800

    修复中文乱码

--- 轻量级Java EE企业应用实战 第4版/01/data.sql
+++ 轻量级Java EE企业应用实战 第4版/01/data.sql
@@ -4,11 +4,10 @@ use javaee;
 
 create table news_inf
 (
- news_id int primary key auto_increment,
- news_title varchar(255)
+    news_id    int primary key auto_increment,
+    news_title varchar(255)
 );
 
 insert into news_inf
-values
-(null , '·è¿ñJavaÁªÃË'),
-(null , 'crazyit.org');
+values (null, 'ç–¯ç‹‚Javaè”ç›Ÿ'),
+       (null, 'crazyit.org');
